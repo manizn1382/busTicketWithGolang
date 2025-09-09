93594bbed09d1933babe71123584076dd3602166
manizn1382 <manizm1382@gmail.com>
2025-09-09 17:16:50 +0330

    .env file added for config
    install module to get some package

--- main.go
+++ main.go
@@ -1,5 +1,41 @@
 package main
 
-func main(){
+import (
+	"database/sql"
+	_ "database/sql"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+
+
+
+
+
+func main() {
 	
+	err := godotenv.Load()
+
+	if err != nil{
+		fmt.Println("error occured.")
+	}
+
+	username  := os.Getenv("userName")
+	password  := os.Getenv("passWord")
+	host      := os.Getenv("host")
+	port      := os.Getenv("port")
+	dbName        := os.Getenv("dbName")
+	dsn       := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", username, password, host, port, dbName)
+
+	db,err := sql.Open("mysql",dsn)
+
+	if err != nil{
+		fmt.Print(err)
+	}
+
+	defer db.Close()
+
+
 }
