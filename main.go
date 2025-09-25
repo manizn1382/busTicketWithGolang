34a3c5cf806a49f13e095b7713c997bef302ac92
manizn1382 <manizm1382@gmail.com>
2025-09-25 15:53:12 +0330

    change prototype of updateUser function and write body of deleteUser and AllUser

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-
-	_"fmt"
+	"fmt"
 	"tick/config"
+	"tick/db"
+	_ "github.com/go-sql-driver/mysql"
+	//_"reflect"
 )
 
 
@@ -14,6 +16,19 @@ import (
 func main() {
 	
 	config.SetEnv()
+	
+	message,err := db.AllUser()
+
+	fmt.Println(*message,err)
+
+	// val := reflect.ValueOf(message)
+    // typ := reflect.TypeOf(message)
+
+    // for i := 0; i < val.NumField(); i++ {
+    //     field := typ.Field(i)
+    //     value := val.Field(i)
+    //     fmt.Printf("%s: %v\n", field.Name, value)
+    // }
 
 
 }
