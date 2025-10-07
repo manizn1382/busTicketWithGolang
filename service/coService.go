f34dc6f22875b4ec126aa78b9ec47e1d57f997cf
manizn1382 <manizm1382@gmail.com>
2025-10-07 17:26:06 +0330

    fill AddCompany function in coService

--- service/coService.go
+++ service/coService.go
@@ -1,6 +1,36 @@
 package service
 
-func AddCompany(){}
+import (
+	"net/http"
+	_"strconv"
+	"tick/model"
+	"tick/db"
+)
+
+func AddCompany(r *http.Request, w http.ResponseWriter){
+	
+	r.ParseForm()
+	supportPhone := r.FormValue("phone")
+	coName := r.FormValue("name")
+	coAddress := r.FormValue("Addr")
+
+	coInfo := model.Company{
+		SupportPhone: supportPhone,
+		Name: coName,
+		Address: coAddress,
+	}
+
+	res := db.AddCompany(coInfo)
+
+	if res != nil{
+		w.Write([]byte (res.Error()))
+	}else{
+		w.Write([]byte ("ok"))
+		w.WriteHeader(200)
+	}
+
+
+}
 
 func ViewCompanyInfo(){}
 
