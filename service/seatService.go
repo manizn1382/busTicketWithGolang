efee665346b1a419d06a5773d8b623534a178cd8
manizn1382 <manizm1382@gmail.com>
2025-10-12 17:45:04 +0330

    write body of seat service functions.

--- service/seatService.go
+++ service/seatService.go
@@ -30,6 +30,47 @@ func ViewSeatStatus(r *http.Request, w http.ResponseWriter){
 
 }
 
-func ReserveSeat(){}
+func ReserveSeat(r *http.Request, w http.ResponseWriter){
+	seat := r.URL.Query().Get("seatNum")
 
-func MakeFree(){}
+	seatInfo,err := db.GetSeatByNumber(seat)
+
+	if err != nil{
+		w.Write([]byte("can't get seat detail with this seat number"))
+		w.WriteHeader(404)
+	}else{
+		seatInfo.Status = "reserve"
+		_,err := db.UpdateSeat(seatInfo)
+
+		if err != nil{
+			w.Write([]byte("can't update seat detail with this seat number"))
+			w.WriteHeader(500)
+		}else{
+			w.Write([]byte("success"))
+			w.WriteHeader(200)
+		}
+	}
+
+}
+
+func MakeFree(r *http.Request, w http.ResponseWriter){
+	seat := r.URL.Query().Get("seatNum")
+
+	seatInfo,err := db.GetSeatByNumber(seat)
+
+	if err != nil{
+		w.Write([]byte("can't get seat detail with this seat number"))
+		w.WriteHeader(404)
+	}else{
+		seatInfo.Status = "Free"
+		_,err := db.UpdateSeat(seatInfo)
+
+		if err != nil{
+			w.Write([]byte("can't update seat detail with this seat number"))
+			w.WriteHeader(500)
+		}else{
+			w.Write([]byte("success"))
+			w.WriteHeader(200)
+		}
+	}
+}
